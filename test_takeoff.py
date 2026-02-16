0f9b77503e9adc1d19297cc1ba01dfe9166d8a34
Shehab <108345797+SchehabM@users.noreply.github.com>
2026-02-16 19:40:01 +0100

    Add files via upload
    Implemented a coverage path-finder / lawn-mower pattern via waypoint based navigation.

--- test_takeoff.py
+++ test_takeoff.py
@@ -10,19 +10,52 @@ if API_control == True:
 else:
 	print("API controll not established")
 client.armDisarm(True,"Drone1")
+def state():
+	return(client.getMultirotorState("Drone1"))
+def x_position(state):
+	return(state.kinematics_estimated.position.x_val)
+def y_position(state):
+	return(state.kinematics_estimated.position.y_val)
+def z_position(state):
+	return(state.kinematics_estimated.position.z_val)
+def x_velocity(state):
+	return(state.kinematics_estimated.linear_velocity.x_val)
+def y_velocity(state):
+	return(state.kinematics_estimated.linear_velocity.y_val)
+def z_velocity(state):
+	return(state.kinematics_estimated.linear_velocity.z_val)
+def position(x_position,y_position,z_position):
+	return (f"current x position:{x_position(state())}, current y position:{y_position(state())}")
+	
 
 
 client.takeoffAsync().join()
 client.moveToPositionAsync(-10, 0, -3, 2).join()
-#velocity_z=state.kinematics_estimated.linear_velocity.z_val
-#position_z=state.kinematics_estimated.position.z_val
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(-10, 2, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(10, 2, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(10, 4, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(-10, 4, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(-10, 6, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(10, 6, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(10, 8, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(-10, 8, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(-10, 10, -3, 2).join()
+print(position(x_position,y_position,z_position))
+client.moveToPositionAsync(10, 10, -3, 2).join()
+print(position(x_position,y_position,z_position))
 
+print("Returning to start position")
 client.moveToPositionAsync(0,0,-3,2).join()
 client.moveToPositionAsync(0,0,1,0.5).join()
-client.landAsync("Drone1").join()
-#state=client.getMultirotorState("Drone1")
-#velocity_x=state.kinematics_estimated.linear_velocity.x_val
-#position_x=state.kinematics_estimated.position.x_val
 client.armDisarm(False,"Drone1")
 print("Motors OFF")
 client.enableApiControl(False,"Drone1")
